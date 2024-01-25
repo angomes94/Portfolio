c8fa00fa5a8ea08e2b4a768597a2727c02c59f59
angomes94 <angomes.94@gmail.com>
2024-01-25 15:36:44 +0000

    image grid improved

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -10,31 +10,31 @@ export default function ProjectCard({ isOrientationStart, project }) {
       <h1 className='text-white font-bold text-xl'>{project.title} {isExpand && <span className=' text-lg font-thin'> {" - " + project.date} </span>}</h1>
       <div className={` w-full py-5 flex transition-all duration-500 ease-in-out justify-center items-center ${isExpand ? ' opacity-100' : ' h-0 opacity-0 overflow-hidden'}`}>
         <div className={` w-full flex flex-col md:flex-row justify-center items-center md:p-10 ${isOrientationStart ? 'md:flex-row' : 'md:flex-row-reverse'}`}>
-          <div className={` grid grid-cols-1 md:grid-cols-2 gap-4 pb-10 md:pb-0 md:pr-10 rounded-xl `}>
-            <div className=' md:max-h-[300px] md:w-auto duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
+          <div className={` grid grid-cols-1 md:grid-cols-2 gap-4 pb-10 md:pb-0 ${isOrientationStart ? "md:pr-10" : "md:pl-10" } rounded-xl `}>
+            <div className=' duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
               <LazyImage
-                className='rounded-xl border-4 border-orange '
+                className='rounded-xl border-4 border-orange md:max-h-[300px] md:w-auto '
                 src={project.img1}
                 alt='Project image 1'
               />
             </div>
-            <div className=' md:max-h-[300px] md:w-auto duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150 '>
+            <div className='  duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150 '>
               <LazyImage
-                className='rounded-xl border-4 border-orange  '
+                className='rounded-xl border-4 border-orange  md:max-h-[300px] md:w-auto '
                 src={project.img2}
                 alt='Project image 2'
               />
             </div>
-            <div className=' md:max-h-[300px] md:w-auto duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
+            <div className='  duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
               <LazyImage
-                className='rounded-xl border-4 border-orange  '
+                className='rounded-xl border-4 border-orange md:max-h-[300px] md:w-auto  '
                 src={project.img3}
                 alt='Project image 3'
               />
             </div>
-            <div className=' md:max-h-[300px] md:w-auto duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
+            <div className='  duration-500 ease-in-out transition-all  md:hover:cursor-zoom-in md:hover:scale-150'>
               <LazyImage
-                className='rounded-xl border-4 border-orange  '
+                className='rounded-xl border-4 border-orange  md:max-h-[300px]  md:w-auto'
                 src={project.img4}
                 alt='Project image 4'
               />
